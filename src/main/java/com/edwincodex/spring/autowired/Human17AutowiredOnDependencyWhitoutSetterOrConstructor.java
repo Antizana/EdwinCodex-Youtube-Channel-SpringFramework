831a512ee55eca6d545b5fd1624a90ad6b900826
Antizana <edwinvillacis@yahoo.com>
2022-03-14 11:55:30 -0500

    Spring Framework Autowired for my YouTube channel

--- src/main/java/com/edwincodex/spring/autowired/Human17AutowiredOnDependencyWhitoutSetterOrConstructor.java
+++ src/main/java/com/edwincodex/spring/autowired/Human17AutowiredOnDependencyWhitoutSetterOrConstructor.java
@@ -3,7 +3,7 @@ package com.edwincodex.spring.autowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
-public class Human {
+public class Human17AutowiredOnDependencyWhitoutSetterOrConstructor {
 
     public void startPumping(){
         if (heart != null){
@@ -20,3 +20,6 @@ public class Human {
     private Heart heart;
 }
 
+//    Your heart is pumping
+//    I'm Alive!
+//    Animal name: Human, number of hearts present: 1
